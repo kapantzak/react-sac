b6a10d2eba0d92cb0e50c59d19e0b671fd068eae
John <kapantzak@gmail.com>
2020-04-12 16:51:46 +0300

    Adds tooltip controls

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -4,7 +4,17 @@ import "./tooltip.css";
 export interface ITooltipProps {}
 
 const Tooltip: FunctionComponent<ITooltipProps> = (props: ITooltipProps) => {
-  return <div className="sac-modal-tooltip">Tools</div>;
+  return (
+    <div className="sac-modal-tooltip">
+      <select>
+        <option value="exists">Exists in</option>
+        <option value="starts">Starts with</option>
+        <option value="starts">Ends with</option>
+        <option value="starts">Regular expression</option>
+      </select>
+      <input type="text" />
+    </div>
+  );
 };
 
 export default Tooltip;
